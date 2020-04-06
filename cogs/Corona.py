2639e6637630ffadf4420a3289caa24b1a646cae
Marcelo Lima <marceloolima92@gmail.com>
2020-04-05 21:42:57 -0300

    Corona emojis, date, better search

--- cogs/Corona.py
+++ cogs/Corona.py
@@ -19,51 +19,51 @@ class Corona(commands.Cog):
     async def corona(self, ctx, *, target_country = None):
         """Fetches and displays Corona Virus current cases from the world"""
 
+        if not target_country:
+            country_flag = "https://cdn.discordapp.com/attachments/587051836050112512/696495396628988014/W31X.gif"
+            url = "http://corona.lmao.ninja/all"
+        else:
+            url = "http://corona.lmao.ninja/countries/" + target_country.lower()
+            country_flag = None
+
         try:
-            r = requests.get("https://corona-stats.online?format=json")
+            r = requests.get(url)
+            corona_stats = r.json()
         except:
             ctx.send("Unable to get information right now. Try again later or contact Cow... idk")
             return
-        
-        if not target_country:
-            corona_stats = r.json()['worldStats']
-            country_flag = "https://upload.wikimedia.org/wikipedia/commons/6/6f/Earth_Eastern_Hemisphere.jpg"
-        else:
-            corona_stats = None
-            countries = r.json()['data']
-            for country in countries:
-                try:
-                    if (country['country'].lower() == target_country.lower() or 
-                    country['countryCode'].lower() == target_country.lower()):
-                        corona_stats = country
-                        country_flag = corona_stats['countryInfo']['flag']
-                        break
-                except:
-                    pass
 
-        if not corona_stats:
-            await ctx.send("Could not find that country.")
+
+        try:
+            if not country_flag:
+                country_flag = corona_stats['countryInfo']['flag']
+            cases_total = "{:,}".format(corona_stats['cases'])
+            cases_new = "{:,}".format(corona_stats['todayCases'])
+            cases_active = "{:,}".format(corona_stats['active'])
+            deaths_total = "{:,}".format(corona_stats['deaths'])
+            deaths_new = "{:,}".format(corona_stats['todayDeaths'])
+            recovered = "{:,}".format(corona_stats['recovered'])
+            updated = datetime.utcfromtimestamp(corona_stats['updated']/1000).strftime('%H:%M %A %d-%m-%Y')
+        except:
+            await ctx.send("Country not found, maybe it has no cases, or you need to type better idk <:peeposhrug:596749393575804937>")
             return
 
+        try:
+            title = corona_stats['country']
+        except:
+            title = "World"
 
-        cases_total = "{:,}".format(corona_stats['cases'])
-        cases_new = "{:,}".format(corona_stats['todayCases'])
-        cases_active = "{:,}".format(corona_stats['active'])
-        deaths_total = "{:,}".format(corona_stats['deaths'])
-        deaths_new = "{:,}".format(corona_stats['todayDeaths'])
-        recovered = "{:,}".format(corona_stats['recovered'])
-
-        embed = discord.Embed(colour=discord.Colour.purple(), title=corona_stats['country'] + " Coronavirus Status", url="https://www.worldometers.info/coronavirus/")
+        embed = discord.Embed(colour=discord.Colour.purple(), title=title + " Coronavirus Status", url="https://www.worldometers.info/coronavirus/")
         
         embed.set_thumbnail(url=country_flag)
-        embed.set_footer(text="🦇")
+        embed.set_footer(text="Last updated: " + updated)
 
-        embed.add_field(name="😷 Total Cases", value=cases_total)
-        embed.add_field(name="🔼 New Cases", value=cases_new)
-        embed.add_field(name="🤢 Active", value=cases_active)
-        embed.add_field(name="💀 Deaths", value=deaths_total)
-        embed.add_field(name="🔺 New Deaths", value=deaths_new)
-        embed.add_field(name="🥳 Recovered", value=recovered)
+        embed.add_field(name="<:totalcases:696506315131846717> Total", value=cases_total)
+        embed.add_field(name="<:casesarrow:696516134962462771> New Cases", value=cases_new)
+        embed.add_field(name="<a:coronacases:696408215675732078> Active", value=cases_active)
+        embed.add_field(name="<:coronadeaths:696408166988120124> Deaths", value=deaths_total)
+        embed.add_field(name="<:deathsarrow:696493553697947690> New Deaths", value=deaths_new)
+        embed.add_field(name="<:coronarecovered:696408101078827049> Recovered", value=recovered)
 
 
         await ctx.send(embed=embed) 
