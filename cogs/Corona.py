c7876523a19c3362c2a77783ebe9358ee32f62da
Marcelo Lima <marceloolima92@gmail.com>
2021-02-23 00:45:52 -0300

    Add percentages and shorter numbers

--- cogs/Corona.py
+++ cogs/Corona.py
@@ -6,6 +6,9 @@ from discord.ext import commands
 
 from datetime import datetime
 
+from millify import millify
+from millify import prettify
+
 class Corona(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -42,7 +45,7 @@ class Corona(commands.Cog):
             data_twoDaysAgo = requests.get(url_2d).json()
             country_flag = country_flag if country_flag else data_today['countryInfo']['flag']
         except:
-            ctx.send("Unable to get information right now. Try again later or contact Cow... idk")
+            await ctx.send("Unable to get information right now. Try again later or contact Cow... idk")
             return
         
         try:
@@ -62,15 +65,40 @@ class Corona(commands.Cog):
                 embed.set_footer(text="Updated less than a min. ago")
             else:
                 embed.set_footer(text="Updated " + str(time) + "min ago")
+                
             
-            embed.add_field(name="<:totalcases:696506315131846717> Total", value="{:,}".format(data['cases']))
-            embed.add_field(name="<:casesarrow:696516134962462771> New Cases", value="{:,}".format(data['todayCases']))
-            embed.add_field(name="<a:coronacases:696408215675732078> Active", value="{:,}".format(data['active']))
-            embed.add_field(name="<:coronadeaths:696408166988120124> Deaths", value="{:,}".format(data['deaths']))
-            embed.add_field(name="<:deathsarrow:696493553697947690> New Deaths", value="{:,}".format(data['todayDeaths']))
-            embed.add_field(name="<:coronarecovered:696408101078827049> Recovered", value="{:,}".format(data['recovered']))
+            
+            todayCases = prettify(data['todayCases'])
+            todayDeaths = prettify(data['todayDeaths'])
+            todayRecovered = prettify(data['todayRecovered'])
+            activeCases = millify(data['active'], precision=2)
+            population = millify(data['population'], precision=2)
+            totalCases = millify(data['cases'], precision=2)
+            tests = millify(data['tests'], precision=2)
+            recovered = millify(data['recovered'], precision=2)
+            totalDeaths = millify(data['deaths'], precision=2)
+            
+            
+            
+            embed.add_field(name="<:casesarrow:696516134962462771> New Cases", value=todayCases)
+            embed.add_field(name="<:deathsarrow:696493553697947690> New Deaths", value=todayDeaths)
+            embed.add_field(name="⛑️ New Recoveries", value=todayRecovered)
+            
+            embed.add_field(name="<:totalcases:696506315131846717> Total Cases", value=totalCases + " **({:.2f}%)**".format(calc_percentage(data['cases'], data['population'])) )
+            embed.add_field(name="<:coronadeaths:696408166988120124> Total Deaths", value=totalDeaths + " **({:.2f}%)**".format(calc_percentage(data['deaths'], data['cases'])) )
+            embed.add_field(name="<:coronarecovered:696408101078827049> Total Recovered", value=recovered + " **({:.2f}%)**".format(calc_percentage(data['recovered'], data['cases'])) )
+            
+            embed.add_field(name="<a:coronacases:696408215675732078> Active Cases", value=activeCases + " **({:.2f}%)**".format(calc_percentage(data['active'], data['population'])) )
+            embed.add_field(name="🧪 Tests", value=tests)
+            embed.add_field(name="<:coronapopulation:813592002293792788> Population", value=population)
+            
+            
             
             return embed
+        
+        def calc_percentage(cases, population):
+            percentage = int(cases)/int(population)*100
+            return percentage
         
         await ctx.send(embed=create_embed(data_today, country_flag, title, "today", time))
         await ctx.send(embed=create_embed(data_yesterday, country_flag, title, "yesterday", time))
